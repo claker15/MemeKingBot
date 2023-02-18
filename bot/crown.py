a92b152e57c9489a3d663278720676ffebe65db5
claker15 <lakercory@gmail.com>
2023-02-18 12:49:16 -0600

    crowning code fix probably

--- bot/crown.py
+++ bot/crown.py
@@ -1,4 +1,4 @@
-import discord
+import disnake
 import logging
 import os
 from dotenv import load_dotenv
@@ -23,15 +23,15 @@ async def crown(ctx):
         res = query.change_king_points(res, guild_id)
         logger.debug("query to update king count of user: {0}".format(res))
         logger.debug("getting channel {0} from guild {1}".format(crown_channels[index], guild_id))
-        channel = discord.utils.get(await guild.fetch_channels(), name=crown_channels[index])
+        channel = disnake.utils.get(await guild.fetch_channels(), name=crown_channels[index])
         logger.debug("found channel name {0} with id: {1} in guild: {2}".format(channel.name, channel.id, guild_id))
         await channel.send("👑{0} is Meme King of the Week 👑".format(member.nick))
-        logger.debug("succesfully crowned {0} for server {1}".format(member.nick, guild_id))
+        logger.debug("Successfully crowned {0} for server {1}".format(member.nick, guild_id))
 
     # add starting points for all people who posted last week
-    users = query.get_users_who_posted_last_week(guild_id)
+    users = query.get_users_who_posted_last_week(ctx.guild_id)
 
     for user in users:
         userid = user[0]
         logger.debug("Giving starting points to user {0}".format(userid))
-        points.starting_points(userid, guild_id)
+        points.starting_points(userid, ctx.guild_id)
