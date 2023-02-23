cf90c1bad544d20e7efbf2af1befd4562cafdb98
Zach <zachary.t.rich@gmail.com>
2023-02-22 22:53:16 -0600

    test crowning

--- bot/bot.py
+++ bot/bot.py
@@ -68,7 +68,7 @@ bot.load_extension("Cogs.sounds")
 
 
 schedule.every().sunday.at('00:01').do(emit_crown, bot)
-schedule.every().wednesday.at('23:50').do(test_schedule, bot)
+schedule.every().wednesday.at('23:57').do(emit_crown, bot)
 stop_run_continuously = run_continuously()
 
 
